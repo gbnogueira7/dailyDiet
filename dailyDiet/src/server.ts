ce0dc7f2f91af1f42e3481a05a0a2d5517950dad
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-02-26 19:40:19 -0300

    feat: configura o knex, sqlite e DATABASE_URL

--- dailyDiet/src/server.ts
+++ dailyDiet/src/server.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
+import { knex } from './database'
 const app = fastify()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
