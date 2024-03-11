7d948554eb449516f68564f2212ed3ebdc1785a5
Gabriel Nogueira <gabrielnogueiraramos@gmail.com>
2024-03-11 19:22:55 -0300

    feat: alter foreignKey in migrate to pg

--- db/migrations/20240226225706_create-meals.ts
+++ db/migrations/20240226225706_create-meals.ts
@@ -2,11 +2,17 @@ import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
-    table.uuid('id').primary()
+    table.uuid('id').defaultTo(knex.raw('uuid_generate_v4()')).primary()
     table.string('name').notNullable()
     table.string('description').notNullable()
     table.boolean('inDiet').notNullable()
-    table.string('userId').unsigned().references('id').inTable('users')
+    table
+      .uuid('userId')
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE') // Adicione isso para atualizar a referência em cascata
+      .notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
   })
 }
